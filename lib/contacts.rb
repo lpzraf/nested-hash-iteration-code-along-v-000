1ab120dd1fc9ba470cbbecb933e91214c890c9d5
Rafael Lopez <lpz.raf@gmail.com>
2018-08-01 01:41:48 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
+contacts.each do |person, contact_details_hash|
+  contact_details_hash.each do |attribute, data|
+    binding.pry
+  end
 end
 end
